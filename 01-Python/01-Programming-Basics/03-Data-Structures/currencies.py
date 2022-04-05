1ff7429a73872f93b2ef0f14107c0b7be3538a37
deepanjalisingh <deepanjalisingh0999@gmail.com>
2022-04-05 14:34:43 +0900

    third challenge of the day

--- 01-Python/01-Programming-Basics/03-Data-Structures/currencies.py
+++ 01-Python/01-Programming-Basics/03-Data-Structures/currencies.py
@@ -1,11 +1,16 @@
 # pylint: disable=missing-docstring
 
-# TODO: add some currency rates
-RATES = {}
+
+RATES = { "USDEUR":0.85,"GBPEUR":1.13,"CHFEUR":0.86,"EURGBP":0.885}
 
 def convert(amount, currency):
     """returns the converted amount in the given currency
     amount is a tuple like (100, "EUR")
-    currency is a string
+    currency is a string like "USD"
     """
-    pass  # YOUR CODE HERE
+    key= amount[1] + currency
+    if key in RATES:
+        currency_value=RATES[key]
+        conversion=round(currency_value*amount[0])
+        return conversion
+    return None
