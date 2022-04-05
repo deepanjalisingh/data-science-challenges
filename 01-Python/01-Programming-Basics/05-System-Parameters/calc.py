86a51307d155eb635bb099d2014f4ac70a8d9d73
deepanjalisingh <deepanjalisingh0999@gmail.com>
2022-04-05 16:35:16 +0900

    fourth challenge of the day

--- 01-Python/01-Programming-Basics/05-System-Parameters/calc.py
+++ 01-Python/01-Programming-Basics/05-System-Parameters/calc.py
@@ -4,9 +4,20 @@ import sys
 
 def main():
     """Implement the calculator"""
-    pass  # YOUR CODE HERE
+    my_list= sys.argv
+    my_list[1]=int(my_list[1])
+    my_list[3]=int(my_list[3])
+    if my_list[2]== "+":
+        return my_list[1] + my_list[3]
+    if my_list[2]== "-":
+        return my_list[1] - my_list[3]
+    #if my_list[2]== "*":
+    return my_list[1] * my_list[3]
+
+
+
+
 
 
 if __name__ == "__main__":
     print(main())
-    
