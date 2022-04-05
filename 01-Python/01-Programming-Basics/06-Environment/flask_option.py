0876e4a3a105db5b6fd349f055210817889546d6
deepanjalisingh <deepanjalisingh0999@gmail.com>
2022-04-05 23:18:44 +0900

    done 6th challenge of the day

--- 01-Python/01-Programming-Basics/06-Environment/flask_option.py
+++ 01-Python/01-Programming-Basics/06-Environment/flask_option.py
@@ -4,7 +4,10 @@ import os
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    if 'FLASK_ENV' in os.environ:
+        environment=os.environ['FLASK_ENV']
+        return f'Starting in {environment} mode...'
+    return "Starting in production mode..."
 
 if __name__ == "__main__":
     print(start())
