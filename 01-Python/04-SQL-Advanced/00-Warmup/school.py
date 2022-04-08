a706510ee455895aae0810a5afd93ae205d0edec
deepanjalisingh <deepanjalisingh0999@gmail.com>
2022-04-08 12:22:20 +0900

    first challenge of the day

--- 01-Python/04-SQL-Advanced/00-Warmup/school.py
+++ 01-Python/04-SQL-Advanced/00-Warmup/school.py
@@ -2,14 +2,17 @@
 
 def students_from_city(db, city):
     """return a list of students from a specific city"""
-    pass  # YOUR CODE HERE
 
+    query = "SELECT * FROM students s WHERE birth_city =?"
+    db.execute(query,(city,))
+    results = db.fetchall()
+    return results
 
 # To test your code, you can **run it** before running `make`
 #   => Uncomment the following lines + run:
 #   $ python school.py
 #
-# import sqlite3
-# conn = sqlite3.connect('data/school.sqlite')
-# db = conn.cursor()
-# print(students_from_city(db, 'Paris'))
+import sqlite3
+conn = sqlite3.connect('data/school.sqlite')
+db = conn.cursor()
+print(students_from_city(db, 'Paris'))
