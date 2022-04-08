d352178f745258584be160615851433a52ccd6a7
deepanjalisingh <deepanjalisingh0999@gmail.com>
2022-04-08 16:58:59 +0900

    second challenge of the day

--- 01-Python/04-SQL-Advanced/01-Query-the-DB/queries.py
+++ 01-Python/04-SQL-Advanced/01-Query-the-DB/queries.py
@@ -2,15 +2,25 @@
 
 def query_orders(db):
     # return a list of orders displaying each column
-    pass  # YOUR CODE HERE
+    results = db.execute("SELECT * FROM Orders o ")
+    results = results.fetchall()
+    return results
 
 def get_orders_range(db, date_from, date_to):
     # return a list of orders displaying all columns with OrderDate between
     # date_from and date_to (excluding date_from and including date_to)
-    pass  # YOUR CODE HERE
+    results = db.execute("SELECT * FROM Orders o2  WHERE OrderDate > ? AND OrderDate <= ?", (date_from, date_to))
+    results = results.fetchall()
+    return results
 
 def get_waiting_time(db):
     # get a list with all the orders displaying each column
     # and calculate an extra TimeDelta column displaying the number of days
     # between OrderDate and ShippedDate, ordered by ascending TimeDelta
-    pass  # YOUR CODE HERE
+    results = db.execute("""
+                         SELECT *, JULIANDAY(ShippedDate) - JULIANDAY(OrderDate) AS TimeDelta
+                         FROM Orders
+                         ORDER BY TimeDelta
+                         """)
+    results = results.fetchall()
+    return results
