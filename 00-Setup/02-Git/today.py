ee66cf39b6a9d7b65f5153d034dc2bda3e339e1e
deepanjalisingh <deepanjalisingh0999@gmail.com>
2022-04-10 21:18:57 +0900

    Fix style issues, should get a 'Good Style' now :pray:

--- 00-Setup/02-Git/today.py
+++ 00-Setup/02-Git/today.py
@@ -1,10 +1,10 @@
-'''
-This is the module docstring
-'''
+"""A module computing buddy pair names for the day"""
 
 
 def my_name_is():
+    """this function returns a string"""
     return 'deepanjali-singh'
 
 def my_buddy_is():
+    """this function returns a string"""
     return 'deepanjali-singh'
