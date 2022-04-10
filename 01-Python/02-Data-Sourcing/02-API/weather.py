9b5cc5b7d8b97d1018901286a6e032d74c5fa45d
deepanjalisingh <deepanjalisingh0999@gmail.com>
2022-04-10 23:54:18 +0900

    api challenge part 1

--- 01-Python/02-Data-Sourcing/02-API/weather.py
+++ 01-Python/02-Data-Sourcing/02-API/weather.py
@@ -9,18 +9,41 @@ BASE_URI = "https://www.metaweather.com"
 
 def search_city(query):
     '''Look for a given city and disambiguate between several candidates. Return one city (or None)'''
-    pass  # YOUR CODE HERE
+    url = "https://www.metaweather.com/api/location/search/?query="+query
+    response = requests.get(url).json()
+    if response and len(response)>0:
+        city = response[0]
+        return city
+    print('City not found')
 
 def weather_forecast(woeid):
     '''Return a 5-element list of weather forecast for a given woeid'''
-    pass  # YOUR CODE HERE
+    url = "https://www.metaweather.com/api/location/"+str(woeid)
+    response = requests.get(url).json()
+    consolidated_weather=response['consolidated_weather']
+    result=[]
+    for weather in consolidated_weather:
+        d={}
+        d['applicable_date']=weather['applicable_date']
+        d['weather_state_name']=weather['weather_state_name']
+        d['max_temp']=weather['max_temp']
+        result.append(d)
+    return result
+
+
 
 def main():
     '''Ask user for a city and display weather forecast'''
     query = input("City?\n> ")
     city = search_city(query)
-    # TODO: Display weather forecast for a given city
-    pass  # YOUR CODE HERE
+    if city:
+        woeid=city['woeid']
+        x=weather_forecast(woeid)
+        print("Here's the weather in " + query)
+        for day in x:
+            print(str(day['applicable_date'])+ ":",
+                  day['weather_state_name'],
+                  str(day['max_temp'])+ "°C")
 
 if __name__ == '__main__':
     try:
