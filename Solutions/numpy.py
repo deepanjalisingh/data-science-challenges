00d4ee4e30d42e9aff9b443e4a7a358d26d349f9
deepanjalisingh <deepanjalisingh0999@gmail.com>
2022-06-16 02:20:22 +0900

    numpy Mean, Var, and Std

---
+++ Solutions/numpy.py
@@ -0,0 +1,15 @@
+import numpy
+arr=input().split(' ')
+n=int(arr[0])
+m=int(arr[1])
+list=[]
+for i in range(0,n):
+    arr=input().split(' ')
+    list.append(arr)
+
+a=numpy.array(list)
+a=a.astype(numpy.float)
+
+print(numpy.mean(a, axis = 1))
+print(numpy.var(a,axis=0))
+print(round(numpy.std(a),11))
